b4edbae7f649da30e94e1d5cf36d196e25aa5101
sfrank90 <sven.frank90@gmail.com>
2015-07-14 16:05:49 +0200

    src added

---
+++ Sven-Frank-Task6/src/jgpstrackedit/view/buttons/SaveMapAsImageButton.java
@@ -0,0 +1,28 @@
+package jgpstrackedit.view.buttons;
+
+import jgpstrackedit.control.UIController;
+import jgpstrackedit.international.International;
+import jgpstrackedit.view.JGPSTrackEdit;
+
+public class SaveMapAsImageButton implements IMenuItemCleanPlugin {
+	private JGPSTrackEdit application;
+
+	@Override
+	public String getMenuNameText() {
+		// TODO Auto-generated method stub
+		return (International.getText("menu.File.Save_Map_View_as_Image") + "...");
+	}
+
+	@Override
+	public void buttonClicked(UIController ui) {
+		// TODO Auto-generated method stub
+		ui.saveMapViewAsImage();
+	}
+
+	@Override
+	public void setApplication(JGPSTrackEdit app) {
+		// TODO Auto-generated method stub
+		this.application = app;
+	}
+
+}
---
+++ Sven-Frank-Task6/src/jgpstrackedit/view/buttons/SaveTrackAsButton.java
@@ -0,0 +1,41 @@
+package jgpstrackedit.view.buttons;
+
+import java.awt.Dimension;
+
+import jgpstrackedit.control.UIController;
+import jgpstrackedit.international.International;
+import jgpstrackedit.view.JGPSTrackEdit;
+
+public class SaveTrackAsButton implements IButtonPlugin{
+	private JGPSTrackEdit application;
+	@Override
+	public Dimension getButtonDimension() {
+		// TODO Auto-generated method stub
+		return new Dimension(20,20);
+	}
+
+	@Override
+	public String getButtonToolTip() {
+		// TODO Auto-generated method stub
+		return International.getText("Save_As");
+	}
+
+	@Override
+	public String getButtonIcon() {
+		// TODO Auto-generated method stub
+		return "/jgpstrackedit/view/icon/page_save.png";
+	}
+
+	@Override
+	public void buttonClicked(UIController ui) {
+		// TODO Auto-generated method stub
+		ui.saveAs();
+	}
+
+	@Override
+	public void setApplication(JGPSTrackEdit app) {
+		// TODO Auto-generated method stub
+		this.application = app;
+	}
+
+}
